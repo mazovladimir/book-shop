becca96b3e2b523273c7ce6aa8bf3298f2dda29f
mazovladimir@gmail.com <root@center.ruzik.net>
2017-03-29 19:09:33 +0300

    Lesson_13 Finished mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:	"www.ruzik.net",
+    port:	587,
+    domain:	"ruzik.net",
+  }
+end
